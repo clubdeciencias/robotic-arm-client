73c0f609dd80c9d3c81339084501acd03d46ac6c
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2018-07-03 20:46:32 +0200

    Añadidos módulos para conexión bluetooth y comprobaciones de adaptador.
    Incorporación de la librería de arduino.

--- app/src/main/java/org/clubdeciencia/roboticarmclient/MainActivity.java
+++ app/src/main/java/org/clubdeciencia/roboticarmclient/MainActivity.java
@@ -1,26 +1,71 @@
 package org.clubdeciencia.roboticarmclient;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.bluetooth.*;
+import android.widget.TextView;
 
 import com.aronbordin.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int REQUEST_BLUETOOTH = 1;
+    boolean BLUETOOTH_STATUS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button + findViewById(R.id.button);
+
+        final Button button = findViewById(R.id.bluetoothButton);
+
+
+        button.setEnabled(false);
+        /*
+        Comprobar si el dispositivo es compatible con bluetooth
+         */
+        final TextView infoTextView = findViewById(R.id.infoTextView);
+        BluetoothAdapter BTAdapter = BluetoothAdapter.getDefaultAdapter();
+        // Phone does not support Bluetooth so let the user know and exit.
+        if (BTAdapter == null) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Not compatible")
+                    .setMessage("Your phone does not support Bluetooth")
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+
+        if (!BTAdapter.isEnabled()) {
+            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, REQUEST_BLUETOOTH);
+        }
+
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setup();
             }
         });
+
+        if(BTAdapter.isEnabled()) {
+            infoTextView.setText("Bluetooth Ready ;)");
+            button.setEnabled(true);
+        }else{
+            infoTextView.setText("Bluetooth Not enabled :/");
+        }
     }
 
 
@@ -29,6 +74,14 @@ public class MainActivity extends AppCompatActivity {
 
     void setup(){
         mBlue.Connect();
-        textSize(30);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == REQUEST_BLUETOOTH) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) BLUETOOTH_STATUS = true;
+        }
     }
 }
