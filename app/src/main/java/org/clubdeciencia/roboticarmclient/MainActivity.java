0955ca68fd55bc1fc833c3d432823a863ae4ea8f
clubdeciencia <contacto@clubdeciencia.org>
2018-07-24 13:34:32 +0200

    Bluetooth Works!

--- app/src/main/java/org/clubdeciencia/roboticarmclient/MainActivity.java
+++ app/src/main/java/org/clubdeciencia/roboticarmclient/MainActivity.java
@@ -4,22 +4,23 @@ import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.UUID;
 
+import static org.clubdeciencia.roboticarmclient.DeviceList.EXTRA_ADDRESS;
+
 public class MainActivity extends AppCompatActivity {
 
-    String address = null;
+    String address = "20:14:12:22:01:47";
     private ProgressDialog progress;
     BluetoothAdapter myBluetooth = null;
     BluetoothSocket btSocket = null;
@@ -34,49 +35,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new ConnectBT().execute(); //Call the class to connect
-
         final Button btButton = findViewById(R.id.bluetoothButton);
         final Button armButton = findViewById(R.id.armButton);
+        final Button disconnectButton = findViewById(R.id.disconnectButton);
+
+        btButton.setEnabled(true);
+        armButton.setEnabled(false);
+        disconnectButton.setEnabled(false);
 
 
-
-        btButton.setEnabled(false);
-        armButton.setEnabled(true);
-
-
-
-
-        /*
-        Comprobar si el dispositivo es compatible con bluetooth
-         */
-        final TextView infoTextView = findViewById(R.id.infoTextView);
-        // Phone does not support Bluetooth so let the user know and exit.
-        if (BTAdapter == null) {
-            new AlertDialog.Builder(this)
-                    .setTitle(R.string.bt_notsupported_title)
-                    .setMessage(R.string.bt_notsupported_msg)
-                    .setPositiveButton(R.string.exit, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            System.exit(0);
-                        }
-                    })
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-        }
-
-        if (!BTAdapter.isEnabled()) {
-            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBT, REQUEST_BLUETOOTH);
-        }
-
+        disconnectButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Disconnect();
+                btButton.setEnabled(true);
+                armButton.setEnabled(false);
+                disconnectButton.setEnabled(false);
+            }
+        });
 
 
         armButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent( MainActivity.this, armControl.class);
+                /*Intent i = new Intent( MainActivity.this, armControl.class);
                 startActivity(i);
+                */
+                led();
                 }
         });
 
@@ -84,42 +69,53 @@ public class MainActivity extends AppCompatActivity {
         btButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setup();
-                if(connected){
-                    armButton.setEnabled(true);
-                }
+
+                new ConnectBT().execute(); //Call the class to connect
+
+                btButton.setEnabled(false);
+                armButton.setEnabled(true);
+                disconnectButton.setEnabled(true);
             }
         });
 
-        final boolean post = btConInfo.post(new Runnable() {
-            @Override
-            public void run() {
-                if (BTAdapter.isEnabled()) {
-                    infoTextView.setText(R.string.bluetooth_ok_msg);
-                    btButton.setEnabled(true);
-                } else {
-                    infoTextView.setText(R.string.bluetooth_disabled_msg);
-                }
-                btConInfo.postDelayed(this, 500); // set time here to refresh textView
-            }
-        });
 
-        private void Disconnect()
+
+    }
+
+    private void Disconnect()
+    {
+        if (btSocket!=null) //If the btSocket is busy
         {
-            if (btSocket!=null) //If the btSocket is busy
+            try
             {
-                try
-                {
-                    btSocket.close(); //close connection
-                }
-                catch (IOException e)
-                { msg("Error");}
-            }
-            finish(); //return to the first layout
+                btSocket.close(); //close connection
 
+            }
+            catch (IOException e)
+            { msg("Error");}
         }
 
+    }
 
+    protected void led()
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write("O".toString().getBytes());
+                msg("1");
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
+
+    private void msg(String s)
+    {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
     }
 
     private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
@@ -129,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute()
         {
-            progress = ProgressDialog.show(ledControl.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+            progress = ProgressDialog.show(MainActivity.this, "Connecting...", "Please wait!!!");  //show a progress dialog
         }
 
         @Override
--- app/src/main/java/org/clubdeciencia/roboticarmclient/armControl.java
+++ app/src/main/java/org/clubdeciencia/roboticarmclient/armControl.java
@@ -1,8 +1,13 @@
 package org.clubdeciencia.roboticarmclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import static org.clubdeciencia.roboticarmclient.MainActivity.*;
 
 public class armControl extends AppCompatActivity {
 
@@ -12,8 +17,17 @@ public class armControl extends AppCompatActivity {
         setContentView(R.layout.activity_arm_control);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final Button button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
 
     }
+
+
 
 }
